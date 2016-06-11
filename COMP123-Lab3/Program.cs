6de63f988c2063a0c4d0a167af726f4bcdbe7d12
R.SIRHC <christopher.r99@live.com>
2016-06-10 21:24:23 -0400

    Add project files.

---
+++ COMP123-Lab3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Author : Christopher Ritchil
+ * Student Number : 300702644
+ * Description : This program demostrate dealing with card decks
+ */
+namespace COMP123_Lab3
+{
+    /**
+     * This class is the "driver" class for our Program
+     * 
+     * @class Program
+     */
+    public class Program
+    {
+
+        /**
+         * the main method for our driver class Program
+         * 
+         * @method Main
+         * @param {string[]} args
+         */
+        public static void Main(string[] args)
+        {
+            // List that represents deck of card
+
+            Deck deck = new Deck(); // create a deck of cards
+            deck.Display(); // display the initial state of the deck
+
+            deck.Shuffle(); // shuffle the deck
+            deck.Display(); // display shuffled deck
+            Console.WriteLine("=====================");
+
+            Card cardDealt = deck.Deal();
+            Console.WriteLine("Card dealt: {0} of {1}", cardDealt.Face, cardDealt.Suit);
+            Console.WriteLine();
+            deck.Display();
+
+
+        }
+
+
+    }
+}
